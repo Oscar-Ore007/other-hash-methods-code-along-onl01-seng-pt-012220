67b9b1bb91973a46a15510ba68d70275f473a4be
Oscar Ore <oscar.ore2011@gmail.com>
2020-02-03 04:08:52 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -8,6 +8,14 @@ groceries = {
   grains: ["rice", "pasta"]
 }
 
+puts groceries.values
+puts "//////////////////////"
+puts groceries.keys
+puts "//////////////////////"
+puts groceries.keys.flatten
+puts "//////////////////////"
+puts groceries.values.flatten.min
+
 def get_the_values(groceries)
   #code your solution here!
 end
