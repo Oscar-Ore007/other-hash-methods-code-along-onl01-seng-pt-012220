683d7d09e15cfa9d9c52659b1a420d0868a5d9d9
Oscar Ore <oscar.ore2011@gmail.com>
2020-02-03 04:10:03 +0000

    Automatically backed up by Learn

--- lib/values.rb
+++ lib/values.rb
@@ -16,6 +16,22 @@ puts groceries.keys.flatten
 puts "//////////////////////"
 puts groceries.values.flatten.min
 
+def get_the_keys(groceries)
+  groceries.keys
+end
+
+get_the_keys(groceries)
+
+
 def get_the_values(groceries)
   #code your solution here!
+  groceries.values 
+  groceries.values.flatten 
+end
+
+get_the_values(groceries)
+
+
+def get_the_min(groceries)
+  groceries.values.flatten.min
 end
